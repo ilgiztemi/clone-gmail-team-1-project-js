8f04f71ceffc59e041a7359644bb9d1d1a092c91
Ilgiz Temi <temirbekov91@gmail.com>
2022-04-03 00:35:50 -0400

    html/css needs to be merged with js files

---
+++ all-emails.js
@@ -0,0 +1,10 @@
+// const primary = require('./primary');
+// const social = require('./social');
+// const promotions = require('./promotions');
+import { primary } from './primary';
+import { promotions } from './promotions';
+import { social } from './social';
+
+const allInbox = [...primary, ...social, ...promotions];
+
+module.exports = { primary, social, promotions, allInbox };
