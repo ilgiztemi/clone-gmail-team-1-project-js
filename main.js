420fab4e694d77d756902689eb464b1fa4cdb42e
Ilgiz Temi <temirbekov91@gmail.com>
2022-04-07 01:46:29 -0400

    changes that we made tonight

--- main.js
+++ main.js
@@ -8,61 +8,60 @@ const pageInfoSpan = document.querySelector('#page-info');
 const prevBtn = document.querySelector('#prev');
 const nextBtn = document.querySelector('#next');
 const supportBtn = document.querySelector('.support-nav-right');
+const spinnerDiv = document.querySelector('#spinner');
 
-supportBtn.addEventListener('click', function (e) {
-  e = document.createElement('p');
-  e.innerText = 'settings';
-  e.style.display = 'block';
-  console.log(e);
-})
-// const spinnerDiv = document.querySelector('#spinner');
+// supportBtn.addEventListener('click', function (e) {
+//   e = document.createElement('p');
+//   e.innerText = 'settings';
+//   e.style.display = 'block';
+//   console.log(e);
+// })
 
 let pageNumber = 0;
 let limit = 15;
-let list = [];
+
 function createMails(mails) {
-  for (let mail of mails) { 
-  // spinnerDiv.style.display = 'block';
-  // console.log(Math.floor(emails.length / limit)); 5
+  // for (let mail of mails) { 
+  // console.log(Math.floor(mails.length / limit)); 5
   // console.log(pageNumber);
 
-//   if (pageNumber === 0) {
-//     prevBtn.disabled = true;
-//   } else {
-//     prevBtn.disabled = false;
-//   }
+  if (pageNumber === 0) {
+    prevBtn.disabled = true;
+  } else {
+    prevBtn.disabled = false;
+  }
 
-//   const lastPage = Math.floor(mails.length / limit);
-//   if (pageNumber === lastPage) {
-//     nextBtn.disabled = true;
-//   } else {
-//     nextBtn.disabled = false;
-//   }
+  const lastPage = Math.floor(mails.length / limit);
+  if (pageNumber === lastPage) {
+    nextBtn.disabled = true;
+  } else {
+    nextBtn.disabled = false;
+  }
 
-//   const start = pageNumber * limit + 1; // 0, 15, 30
-//   const end = (pageNumber + 1) * limit; // 15, 30, 45
+  const start = pageNumber * limit; // 0, 15, 30
+  const end = (pageNumber + 1) * limit; // 15, 30, 45
 
-  // const partialMails = mails.slice(start, end); //[]
+  const partialMails = mails.slice(start, end); //[]
+    console.log(partialMails);
+  pageInfoSpan.innerText = `${start+1}-${end} of ${mails.length}`;
 
-  // pageInfoSpan.innerText = `${start}-${end} of ${mails.length}`;
-
-//   setTimeout(() => {
-//     for (let mail of partialMails) {
-//       const mailTime = mail.date.toLocaleString('en-US', {
-//         hour: 'numeric',
-//         minute: 'numeric',
-//         hour12: true,
-//       });
+  setTimeout(() => {
+    for (let mail of mails) {
+      const mailTime = mail.date.toLocaleString('en-US', {
+        hour: 'numeric',
+        minute: 'numeric',
+        hour12: true,
+    })
         const eachMail = `<li class="mail-item">
           <div class="mail-icons-left">
             <input type="checkbox" name="mail" id="mail">
             <i class="far fa-star"></i>
             <i class="far fa-bookmark"></i>
-            <h4 class="title">Slack</h4>
+            <h4 class="title">${mail.senderName}</h4>
         </div>
         <div class="mail-content">
-            <p class="subject">Slack account sign-in form a new device
-                <span class="sub-text"> - Slack account signed-in from a new android device</span>
+            <p class="subject">${mail.messageTitle}
+                <span class="sub-text"> - ${mail.messages[0].message}</span>
             </p>
         </div>
         <div class="mail-icons-right">
@@ -71,27 +70,22 @@ function createMails(mails) {
             <i class="fas fa-envelope-open"></i>
             <i class="fas fa-clock"></i>
         </div>
-        <div class="time-date">8:45PM</div>  
+        <div class="time-date">${mailTime}</div>  
         </li>`;
       mailList.innerHTML += eachMail;
     }
+    spinnerDiv.style.display = 'none';
+  },1000)
   }
-  function addList (mail) {
-      for (let i of mail) {
-          list += mail[i];
-      }
-  };
-  addList(list);
-createMails(primary);
-console.log(list);
-// prevBtn.addEventListener('click', function () {
-//   mailList.innerHTML = '';
-//   pageNumber--;
-//   createMails(primary);
-// });
+  createMails(primary);
+  prevBtn.addEventListener('click', function () {
+    mailList.style.display = "";
+    pageNumber--;
+    createMails(primary);
+  });
 
-// nextBtn.addEventListener('click', function () {
-//   mailList.innerHTML = '';
-//   pageNumber++;
-//   createMails(primary);
-// });
+nextBtn.addEventListener('click', function () {
+  mailList.style.display = "";
+  pageNumber++;
+  createMails(primary);
+});
